328f3ad4343a4ffbba256f5f83489c683e0b4917
NekTaf <nektariostaf@gmail.com>
2026-02-25 12:17:29 +0000

    Remove low pass butterworth and moving average filters; Add median filter

--- mocap_pose_to_odom/config.py
+++ mocap_pose_to_odom/config.py
@@ -3,9 +3,4 @@ from dataclasses import dataclass
 @dataclass
 class MocapCfg:
     frame: str = 'world'
-    buffer_size: int = 10
-
-    # Low pass filter params
-    mocap_rate_hz = 100.0
-    lpf_order = 2
-    lpf_cutoff_hz = 5.0
+    buffer_size: int = 15
